9a03cfa810195a72cf99eea209d4fda464728257
Florin Cazacu <florin.cazacu@radcom.com>
2024-01-30 19:27:37 +0200

    Add checkout and star review BE

--- spring-boot-library/src/main/java/com/example/springbootlibrary/config/CustomDataRestConfig.java
+++ spring-boot-library/src/main/java/com/example/springbootlibrary/config/CustomDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootlibrary.config;
 
 import com.example.springbootlibrary.entity.Book;
+import com.example.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -21,8 +22,10 @@ public class CustomDataRestConfig implements RepositoryRestConfigurer {
 			HttpMethod.PUT};
 
 		config.exposeIdsFor(Book.class);
+		config.exposeIdsFor(Review.class);
 
 		disableHttpMethods(Book.class, config, unsupportedActions);
+		disableHttpMethods(Review.class, config, unsupportedActions);
 
 		/* Configure CORS Mapping */
 		cors.addMapping(config.getBasePath() + "/**")
