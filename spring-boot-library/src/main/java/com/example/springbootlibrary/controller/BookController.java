89163bbb810ab7868bead89792318ebc4bfa77cd
Florin Cazacu <florin.cazacu@radcom.com>
2024-02-05 11:37:12 +0200

    Add checkout functionality BE

---
+++ spring-boot-library/src/main/java/com/example/springbootlibrary/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.example.springbootlibrary.controller;
+
+import com.example.springbootlibrary.entity.Book;
+import com.example.springbootlibrary.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+	private final BookService bookService;
+
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+
+	@GetMapping("/secure/currentloans/count")
+	public int currentLoansCount() {
+		String userEmail = "testuser@email.com";
+
+		return bookService.currentLoansCount(userEmail);
+	}
+
+	@GetMapping("/secure/ischeckedout/byuser")
+	public Boolean checkoutBookByUser(@RequestParam Long bookId) {
+		String userEmail = "testuser@email.com";
+
+		return bookService.checkoutBookByUser(userEmail, bookId);
+	}
+
+	@PutMapping("/secure/checkout")
+	public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+		String userEmail = "testuser@email.com";
+
+		return bookService.checkoutBook(userEmail, bookId);
+	}
+}
