a31d15d1b3e842902467b6d2f8cc6f889e924337
Florin Cazacu <florin.cazacu@radcom.com>
2024-02-16 10:00:35 +0200

    Add Message functionality BE

--- spring-boot-library/src/main/java/com/example/springbootlibrary/service/MessageService.java
+++ spring-boot-library/src/main/java/com/example/springbootlibrary/service/MessageService.java
@@ -2,8 +2,11 @@ package com.example.springbootlibrary.service;
 
 import com.example.springbootlibrary.dao.MessageRepository;
 import com.example.springbootlibrary.entity.Message;
+import com.example.springbootlibrary.requestmodels.AdminQuestionRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -19,6 +22,20 @@ public class MessageService {
 		Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
 		message.setUserEmail(userEmail);
 
+		messageRepository.save(message);
+	}
+
+	public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+		Optional<Message> messageOptional = messageRepository.findById(adminQuestionRequest.getId());
+
+		if (messageOptional.isEmpty()) {
+			throw new Exception("Message not found");
+		}
+		Message message = messageOptional.get();
+
+		message.setAdminEmail(userEmail);
+		message.setResponse(adminQuestionRequest.getResponse());
+		message.setClosed(true);
 		messageRepository.save(message);
 	}
 }
